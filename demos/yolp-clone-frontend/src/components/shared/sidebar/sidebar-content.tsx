021b944dd24b4e92492e37c1dd01a364f37bc385
Cesaire <cesaire.civil@gmail.com>
2025-05-01 18:37:43 -0400

    3ed commit

--- demos/yolp-clone-frontend/src/components/shared/sidebar/sidebar-content.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface SidebarContainerProps {
-  children: React.ReactNode;
-}
-
-export const SidebarContent = ({ children }: SidebarContainerProps) => {
-  return <div className="flex flex-col gap-y-10">{children}</div>;
-};
--- demos/yolp-clone-frontend/src/features/auth/models/auth.ts
+++
@@ -1,5 +0,0 @@
-export interface Auth {
-  id: number;
-  username: string;
-  role: "USER" | "ADMIN";
-}
